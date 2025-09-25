35d149c0395f98d02d8c86afe452690ec4d382f4
Azizbek2333 <250424@newuu.uz>
2025-09-25 23:49:29 +0500

    Create problem9.cpp

---
+++ problem9.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int  main() {
+    int  a;
+    cout << "Ener  speed:";
+    cin  >> a ;
+    if (a<20)  {
+        cout << " Too slow";
+    }
+    if (a>80)  {
+        cout << " Too fast";
+    }
+    else  cout << "right";
+    return 0;
+}
