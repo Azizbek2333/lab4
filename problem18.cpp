88edfdb79598243c30e3ef2256562748640401b5
Azizbek2333 <250424@newuu.uz>
2025-11-11 22:49:32 +0500

    Update problem18.cpp

--- problem18.cpp
+++ problem18.cpp
@@ -1,34 +1,16 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-    char choice;
-    cout << "Choose a language for greeting:" << endl;
-    cout << "u - Uzbek" << endl;
-    cout << "e - English" << endl;
-    cout << "r - Russian" << endl;
-    cout << "g - German" << endl;
-    cout << "Enter your choice: ";
-    cin >> choice;
-    switch (choice) {
-        case 'u':
-        case 'U':
-            cout << "Salom!" << endl;
-            break;
-        case 'e':
-        case 'E':
-            cout << "Hello!" << endl;
-            break;
-        case 'r':
-        case 'R':
-            cout << "Privet!" << endl;
-            break;
-        case 'g':
-        case 'G':
-            cout << "Hallo!" << endl;
-            break;
-        default:
-            cout << "Invalid choice! Please enter u, e, r, or g." << endl;
+    char a;
+    cin>>a;
+    switch(a) {
+        case 'u':cout<<"Salom";break;
+        case 'e':cout<<"hELLO";break;
+        case 'r':cout<<"Privet";break;
+        case 'g':cout<<"Hay gitler";break;
+        default:cout<<"I don't know that language bro";break;
     }
+
     return 0;
+
 }
